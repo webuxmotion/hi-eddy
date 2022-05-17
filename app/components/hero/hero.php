7268c3ee1d78c4034a427e9f47e68aaa93739e0e
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-05-17 11:51:31 +0300

    Initial commit

---
+++ app/components/hero/hero.php
@@ -0,0 +1,11 @@
+<div class="hero">
+  <?=$this->component('logo')?>
+  <h1>HI-<i>EDDY</i></h1>
+  <p class="hero__text">
+    hi-eddy - це освітня платформа.
+  </p>
+  <?=$this->component('button', [
+    "title" => "Дивитись документацію",
+    "href" => "/docs",
+  ])?>
+</div>
---
+++ app/pages/Docs/index.php
@@ -0,0 +1,42 @@
+<style>
+.docs img {
+    max-width: 800px;
+}
+</style>
+<div class="docs">
+<h1>HI-<i>EDDY</i></h1>
+    <p>hi-eddy - це освітня платформа.</p>
+    <img src="/docs/img/hero-image.png" />
+    <br>
+    <img src="/docs/img/image-1.png" />
+    <br>
+    <img src="/docs/img/image-2.png" />
+    <br>
+    <img src="/docs/img/lesson-1.png" />
+    <br>
+    <img src="/docs/img/lesson-2.png" />
+    <br>
+    <img src="/docs/img/lesson-3.png" />
+    <br>
+    <img src="/docs/img/sketch-main-page.jpeg" />
+    <br>
+    <img src="/docs/img/sketch-db-tables-1.jpeg" />
+    <br>
+    <img src="/docs/img/sketch-db-tables-2.jpeg" />
+    <br>
+    <img src="/docs/img/sketch-functionality-1.jpeg" />
+    <br>
+    <img src="/docs/img/sketch-functionality-2.jpeg" />
+    <br>
+    <img src="/docs/img/sketch-functionality-3.jpeg" />
+    <br>
+    <img src="/docs/img/face-1.jpeg" />
+    <br>
+    <img src="/docs/img/face-2.jpeg" />
+    <br>
+    <img src="/docs/img/face-3.jpeg" />
+    <br>
+    <img src="/docs/img/face-4.jpeg" />
+    <br>
+    <img src="/docs/img/face-5.jpeg" />
+</div>
