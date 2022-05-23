d690be40657314b24ba4a8761353db6d2a33bcf0
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-05-23 13:45:22 +0300

    Hero

--- app/components/hero/hero.php
+++ app/components/hero/hero.php
@@ -1,8 +1,13 @@
-<div class="hero" style="background-image: url('/hero.png');">
-  <h2 class="hero__text">
-    <?=__('main_index_lets_make_site')?>
-  </h2>
-  <a href="<?=baseUrl()?>lessons/create-simple-html-page" class="hero__button">
-    <?=__('main_index_begin')?>
-  </a>
+<div class="hero">
+  <div class="hero__image-main" style="background-image: url('/hero-images/main.png');"></div>
+  <div class="hero__image-sea" style="background-image: url('/hero-images/sea.png');"></div>
+  <div class="hero__image-duck" style="background-image: url('/hero-images/duck.png');"></div>
+  <div class="hero__content">
+    <h2 class="hero__text">
+      <?=__('main_index_lets_make_site')?>
+    </h2>
+    <a href="<?=baseUrl()?>lessons/create-simple-html-page" class="hero__button">
+      <?=__('main_index_begin')?>
+    </a>
+  </div>
 </div>
