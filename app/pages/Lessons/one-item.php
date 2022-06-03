dbc35047b0027c9e11971b77ccce08da008f4296
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-06-03 18:41:34 +0300

    getParam

--- app/pages/Lessons/one-item.php
+++ app/pages/Lessons/one-item.php
@@ -27,8 +27,8 @@
     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
     
     var disqus_config = function () {
-        this.page.url = "<?=baseUrl()?>lessons/<?=$item['slug']?>";  // Replace PAGE_URL with your page's canonical URL variable
-        this.page.identifier = "<?=$item['slug']?>"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        this.page.url = "<?=baseUrl()?>lessons/<?=getParam($item, 'slug')?>";  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = "<?=getParam($item, 'slug')?>"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
     };
     
     (function() { // DON'T EDIT BELOW THIS LINE
