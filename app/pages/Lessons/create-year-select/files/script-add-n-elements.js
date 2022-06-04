bc28c17dd82553d7299a75da6d9f85a412342c4f
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-06-04 11:55:27 +0300

    Fix

--- app/pages/Lessons/create-year-select/files/script-add-n-elements.js
+++ app/pages/Lessons/create-year-select/files/script-add-n-elements.js
@@ -1,5 +1,5 @@
 for (let year = 2010; year <= 2100; year++) {
-  var optionElement = document.createElement('option');
+  const optionElement = document.createElement('option');
   optionElement.setAttribute('value', year);
   optionElement.innerHTML = year;
 
