751a768ef80877c65112a67fc3f49e04f883f8dc
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-06-11 08:50:37 +0300

    Customers Power

--- app/pages/Service/about.php
+++ app/pages/Service/about.php
@@ -3,9 +3,48 @@
     <h1>Про нас</h1>
   </div>
   <div class="service__content">
-    <h2>Місія академії:</h2>
+    <h2>Місія академії</h2>
     <p>
       Робити процесс навчання максимально простим та цікавим.
     </p>
+
+    <h2>Наша слабкість - ваша сила</h2>
+
+    <p>
+      В цьому розділі ми розповідаємо про наші слабкі місця.
+    </p>
+
+    <h3>
+    Місце номер 1
+    </h3>
+    <p>
+      Якщо ви коли-небудь щось купували на нашому сайті,
+      то тепер можете активовувати 10-денний пробний тарифний план
+      кожні 50 днів.
+    </p>
+    <p>
+      А якщо ви принципово нічого не хочете купувати, 
+      можете активовувати 10-денний пробний тарифний план
+      кожні 200 днів.
+    </p>
+    <p>
+      Це видно на схемі нашого user-flow:
+    </p>
+    <div>
+    <img src="/images-about/hi-eddys-customers-power-1.png" alt="hi-eddys-customers-power-1">
+    </div>
+    
+    <p>
+      А це збільшене зображення:
+    </p>
+    <div>
+    <img src="/images-about/hi-eddys-customers-power-2.png" alt="hi-eddys-customers-power-2">
+    </div>
+    
+    <p>
+      Тут можете поклацати наш user-flow, може знайдете для себе ще 
+      чогось цікавенького:
+    </p>
+    <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FusZq12z5EdkF2MNk1HhUAx%2FShopping-App-User-Flow-(Community)%3Fnode-id%3D0%253A1" allowfullscreen></iframe>
   </div>
 </div>
